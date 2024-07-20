efbd77eb06036a85139faf3324367fce5ec2d459
Denis Ilchishin <ilchishin.denis@gmail.com>
2024-07-20 11:47:32 +0700

    first commit

---
+++ index.js
@@ -0,0 +1,19 @@
+export default (options) => ({
+  name: 'fix-extensions',
+  setup(build) {
+    build.onLoad({ filter: /.*/ }, async (args) => {
+      console.log(args)
+      if (args.namespace === 'file') {
+        const contents = await fsp.readFile(args.path, {
+          encoding: 'utf-8',
+        })
+        const newContents = contents.replace(
+          /(import )(.*)(from '\.\/)(.*)(\.ts')/gm,
+          "$1$2$3$4.js'",
+        )
+        return { contents: newContents, loader: 'ts' }
+      }
+      return {}
+    })
+  },
+})
